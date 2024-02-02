dbec3ef93a2bfe7bd62f34c23b681ec1bfd9ac5f
reassign <snowboardz125@gmail.com>
2024-02-01 21:30:01 -0500

    Initial Commit

---
+++ LiveStreaming/LiveStreamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveStreamingApp.swift
+//  LiveStreaming
+//
+//  Created by Alex Mierzejewski on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveStreamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
